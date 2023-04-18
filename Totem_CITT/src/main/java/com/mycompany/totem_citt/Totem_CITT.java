0c966845a69eb1d27b74602d843df1f214e414b5
DiegoRiosGo <die.rios@duocuc.cl>
2023-04-18 18:44:31 -0400

    primera etapa

---
+++ Totem_CITT/src/main/java/com/mycompany/totem_citt/Totem_CITT.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.totem_citt;
+
+/**
+ *
+ * @author pc3
+ */
+public class Totem_CITT {
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
