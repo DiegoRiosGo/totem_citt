f70229dc93beecb57b2ba77493b87b0711fb4aac
DiegoRiosGo <die.rios@duocuc.cl>
2023-04-19 19:05:29 -0400

    primerabase

--- Proyecto_citt/src/proyecto_citt/Proyecto_citt.java
+++ Proyecto_citt/src/proyecto_citt/Proyecto_citt.java
@@ -4,6 +4,8 @@
  */
 package proyecto_citt;
 
+import proyecto_citt.vista.Menu;
+
 /**
  *
  * @author diego
@@ -14,7 +16,7 @@ public class Proyecto_citt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         new Menu().setVisible(true);
     }
     
 }
